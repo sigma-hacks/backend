9c066444e510a3631832757b6f47eb95a1c43ef8
Stanislav Strizhkov <d1d2c0d1@gmail.com>
2023-09-16 19:13:57 +0300

    Create routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthorizationController;
 use App\Http\Controllers\BusRouteStationController;
 use App\Http\Controllers\CardTariffController;
 use App\Http\Controllers\CompaniesController;
+use App\Http\Controllers\CompanyServicesController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TestingController;
 use Illuminate\Http\Request;
@@ -80,5 +81,25 @@ Route::prefix('companies')->group(function () {
     Route::middleware(['auth:sanctum','roles:admin'])->group(function () {
         Route::post('', [CompaniesController::class, 'store'])->name('companies.create');
         Route::patch('{id}', [CompaniesController::class, 'update'])->name('companies.create');
+    });
+});
+
+/**
+ * Company routes
+ */
+Route::prefix('company')->group(function () {
+
+    /**
+     * Routes for company services
+     */
+    Route::prefix('services')->group(function () {
+        Route::get('', [CompanyServicesController::class, 'index'])->name('company.services.list');
+        Route::get('{id}', [CompanyServicesController::class, 'single'])->name('company.services.single');
+
+        Route::middleware(['auth:sanctum', 'roles:partner,admin'])->group(function () {
+            Route::post('', [CompanyServicesController::class, 'store'])->name('company.services.create');
+            Route::patch('{id}', [CompanyServicesController::class, 'update'])->name('company.services.update');
+            Route::delete('{id}', [CompanyServicesController::class, 'delete'])->name('company.services.delete');
+        });
     });
 });
