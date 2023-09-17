6799caefbf2e3e5ac4fb04c4e11bf029162eb1be
Stanislav Strizhkov <d1d2c0d1@gmail.com>
2023-09-17 11:23:13 +0300

    Fix create token route

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -16,7 +16,12 @@ class AuthorizationController extends BaseController
      */
     public function createPersonalToken(Request $request): JsonResponse
     {
-        $token = $request->user()?->createToken($request->token_name);
+
+        if( !$request->has('token_name') ) {
+            return $this->sendError('Field "token_name" is required and can\'t be empty', '', 412);
+        }
+
+        $token = $request->user()?->createToken($request->input('token_name'));
 
         return $this->sendResponse([
             'token' => $token?->plainTextToken
