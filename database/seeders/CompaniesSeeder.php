52e27e2f604e858ec91f3482cb216f352000c46e
Stanislav Strizhkov <d1d2c0d1@gmail.com>
2023-09-17 13:54:29 +0300

    Add companies seeder

--- database/seeders/CompaniesSeeder.php
+++ database/seeders/CompaniesSeeder.php
@@ -2,18 +2,35 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Exception;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CompaniesSeeder extends Seeder
 {
 
-    static array $roles = [
+    static array $companies = [
         [
-            'is_guest' => true,
-            'name' => 'Гость',
-            'code' => 'guest'
-        ]
+            'is_active' => true,
+            'name' => 'Автотранспорт',
+            'code' => 'buses',
+            'photo' => '',
+            'description' => 'Автотранспортные компании объединенные в единую сеть'
+        ],
+        [
+            'is_active' => true,
+            'name' => 'Деловые линии',
+            'code' => 'bussines-lines',
+            'photo' => 'https://static.insales-cdn.com/files/1/7913/14933737/original/%D0%94%D0%B5%D0%BB%D0%BE%D0%B2%D1%8B%D0%B5_%D0%BB%D0%B8%D0%BD%D0%B8%D0%B8_0c75131f275f249e506ea398b3515c92.jpg',
+            'description' => 'Услуги населению'
+        ],
+        [
+            'is_active' => true,
+            'name' => 'Снежная деревня',
+            'code' => 'snow-village',
+            'photo' => 'https://snowderevnya.ru/assets/img/logo-sneg.png',
+            'description' => 'Развлечения и досуг'
+        ],
     ];
 
     /**
@@ -21,6 +38,13 @@ class CompaniesSeeder extends Seeder
      */
     public function run(): void
     {
-
+        foreach (self::$companies as $key => $company) {
+            try {
+                $company['id'] = $key;
+                DB::table('companies')->insert($company);
+            } catch (Exception $e) {
+                // empty catch =(
+            }
+        }
     }
 }
